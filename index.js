4bf1a017bbaf9e5e3b286377c6f08997e9f75fdb
joshc0des <joshuahooper3@gmail.com>
2023-01-21 15:11:57 -0600

    fixed 3.9

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 morgan.token('post-data', function showData (req, res) {
   if (req.method === 'POST') {
     return JSON.stringify(req.body)
@@ -7,6 +8,7 @@ morgan.token('post-data', function showData (req, res) {
 })
 const app = express()
 app.use(express.json());
+app.use(cors())
 app.use(morgan(
   ':method :url :status :res[content-length] - :response-time ms :post-data'
   ))
