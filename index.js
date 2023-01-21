9d5163a432bb399d789fc9653f71c44ee5234d37
joshc0des <joshuahooper3@gmail.com>
2023-01-21 16:07:56 -0600

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 morgan.token('post-data', function showData (req, res) {
   if (req.method === 'POST') {
@@ -7,6 +8,7 @@ morgan.token('post-data', function showData (req, res) {
 })
 const app = express()
 app.use(express.json());
+app.use(cors())
 app.use(morgan(
   ':method :url :status :res[content-length] - :response-time ms :post-data'
   ))
