f2c6e2614daaed964473e454e085d300036f7581
joshc0des <joshuahooper3@gmail.com>
2023-01-22 17:05:48 -0600

    3.11

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 // })
 const app = express()
 app.use(express.json());
+app.use(express.static('build'))
 app.use(cors())
 // app.use(morgan(
 //   ':method :url :status :res[content-length] - :response-time ms :post-data'
