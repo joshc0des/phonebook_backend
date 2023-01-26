1b1255a32c22369964a41b35c057d71c1a4e6708
joshc0des <joshuahooper3@gmail.com>
2023-01-25 20:59:41 -0600

    3.13

--- index.js
+++ index.js
@@ -17,28 +17,29 @@ app.use(cors())
 //   ))
 
 
-let persons = [
-  { 
-    name: "Arto Hellas", 
-    number: "040-123456",
-    id: 1
-  },
-  { 
-    name: "Ada Lovelace", 
-    number: "39-44-5323523",
-    id: 2
-  },
-  { 
-    name: "Dan Abramov", 
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }
-]
+// removed in 3.13
+// let persons = [
+//   { 
+//     name: "Arto Hellas", 
+//     number: "040-123456",
+//     id: 1
+//   },
+//   { 
+//     name: "Ada Lovelace", 
+//     number: "39-44-5323523",
+//     id: 2
+//   },
+//   { 
+//     name: "Dan Abramov", 
+//     number: "12-43-234345",
+//     id: 3
+//   },
+//   {
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//     id: 4
+//   }
+// ]
 
 
 const isDup = (new_p) => {
